5c7f4ac2e8319e0882ca7dc7b769a6f9852dfe4f
topisark <topi.sarkkinen@gmail.com>
2018-01-30 21:05:56 +0200

    Set cors headers

--- shared/responses.js
+++ shared/responses.js
@@ -1,5 +1,10 @@
+const corsHeaders = {
+  "Access-Control-Allow-Origin" : "*"
+};
+
 export const successResponse = (data) => {
   return {
+    headers: corsHeaders,
     statusCode: 200,
     body: JSON.stringify({
       data
@@ -9,6 +14,7 @@ export const successResponse = (data) => {
 
 export const errorResponse = (error) => {
   return {
+    headers: corsHeaders,
     statusCode: error.status || 500,
     body: JSON.stringify({
       error
