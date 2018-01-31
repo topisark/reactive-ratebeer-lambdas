d8a05327d9a0c62cfc67f7892eba2264e30c25f8
topisark <topi.sarkkinen@gmail.com>
2018-01-31 21:04:33 +0200

    Tests for beers lambda

---
+++ tests/beers.test.js
@@ -0,0 +1,58 @@
+import { handler } from '../lambdas/beers';
+import { insertTestBeer, cleanUpTestData } from './utils/database-helper';
+
+describe('Beers lambda', () => {
+
+  const lambdaEvent = {};
+  const lambdaContext = {};
+
+  beforeAll(() => {
+    return insertTestBeer();
+  });
+
+  afterAll(() => {
+    return cleanUpTestData();
+  });
+
+  describe('Successful invocation', () => {
+    test('should not return an error', (done) => {
+      handler(lambdaEvent, lambdaContext, (err, result) => {
+        expect(err).toBeNull();
+        done();
+      })
+    });
+
+    test('should return a response', (done) => {
+      handler(lambdaEvent, lambdaContext, (err, result) => {
+        expect(result).toBeDefined();
+        done();
+      })
+    });
+
+    test('should return a response with status 200', (done) => {
+      handler(lambdaEvent, lambdaContext, (err, result) => {
+        expect(result.statusCode).toEqual(200);
+        done();
+      })
+    });
+
+    test('should return a response with JSON body', (done) => {
+      handler(lambdaEvent, lambdaContext, (err, result) => {
+        expect(typeof result.body).toEqual('string');
+        const parsedBody = JSON.parse(result.body);
+        expect(parsedBody).toBeDefined();
+        done();
+      })
+    });
+
+    test('response body should include an array of data', (done) => {
+      handler(lambdaEvent, lambdaContext, (err, result) => {
+        const parsedBody = JSON.parse(result.body);
+        expect(Array.isArray(parsedBody.data)).toEqual(true);
+        done();
+      })
+    });
+
+  });
+
+});
