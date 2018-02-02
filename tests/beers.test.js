03bc648f9ef9851074605a2ba8c61ead7b087cca
topisark <topi.sarkkinen@gmail.com>
2018-02-02 11:41:25 +0200

    Fix tests

--- tests/beers.test.js
+++ tests/beers.test.js
@@ -1,56 +1,52 @@
-import { handler } from '../src/lambdas/beers/handler';
+import { beers } from '../src/lambdas/beers/handler';
 import { insertTestBeer, cleanUpTestData } from './utils/database-helper';
 
 describe('Beers lambda', () => {
 
-  const lambdaEvent = {};
+  const getBeersEvent = { httpMethod: 'GET' };
   const lambdaContext = {};
 
-  beforeAll(() => {
-    return insertTestBeer();
-  });
+  beforeAll(() => insertTestBeer());
 
-  afterAll(() => {
-    return cleanUpTestData();
-  });
+  afterAll(() => cleanUpTestData());
 
-  describe('Successful invocation', () => {
+  describe('GET beers invocation', () => {
     test('should not return an error', (done) => {
-      handler(lambdaEvent, lambdaContext, (err, result) => {
+      beers(getBeersEvent, lambdaContext, (err) => {
         expect(err).toBeNull();
         done();
-      })
+      });
     });
 
     test('should return a response', (done) => {
-      handler(lambdaEvent, lambdaContext, (err, result) => {
+      beers(getBeersEvent, lambdaContext, (err, result) => {
         expect(result).toBeDefined();
         done();
-      })
+      });
     });
 
     test('should return a response with status 200', (done) => {
-      handler(lambdaEvent, lambdaContext, (err, result) => {
+      beers(getBeersEvent, lambdaContext, (err, result) => {
         expect(result.statusCode).toEqual(200);
         done();
-      })
+      });
     });
 
     test('should return a response with JSON body', (done) => {
-      handler(lambdaEvent, lambdaContext, (err, result) => {
+      beers(getBeersEvent, lambdaContext, (err, result) => {
         expect(typeof result.body).toEqual('string');
         const parsedBody = JSON.parse(result.body);
         expect(parsedBody).toBeDefined();
         done();
-      })
+      });
     });
 
     test('response body should include an array of data', (done) => {
-      handler(lambdaEvent, lambdaContext, (err, result) => {
+      beers(getBeersEvent, lambdaContext, (err, result) => {
         const parsedBody = JSON.parse(result.body);
         expect(Array.isArray(parsedBody.data)).toEqual(true);
         done();
-      })
+      });
     });
 
   });
